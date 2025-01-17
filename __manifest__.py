caac20c12e83e28e48e9fb1e8c204975a5aea117
héric Libong <hericlibong@gmail.com>
2025-01-17 06:48:37 +0100

    Initial commit: create base structure for Odoo DAM module

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Odoo DAM Module',
+    'version': '1.0',
+    'summary': 'Digital Asset Management module for Odoo',
+    'description': """
+        This module provides Digital Asset Management (DAM) capabilities within Odoo.
+        It allows users to manage, organize, and retrieve digital assets efficiently.
+    """,
+    'author': 'Heric Libong',
+    'website': 'https://github.com/hericlibong/odoo-dam-module/wiki',
+    'category': 'Tools',
+    'depends': [],
+    'data': [
+        # List your data files here
+    ],
+    'demo': [
+        # List your demo files here
+    ],
+    'installable': True,
+    'application': True,
+    "license": "LGPL-3",
+}
